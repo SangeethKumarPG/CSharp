52253529a3819fe27f0aeadd47a80ff406afcd8c
sangeethkumarpg <pgsangeethkumar@gmai.com>
2025-03-13 07:28:31 +0530

    first commit

---
+++ Addition/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter the first number : ");
+double firstNumber = 0.0;
+double secondNumber = 0.0;
+double sum = 0.0;
+firstNumber = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number : ");
+secondNumber = double.Parse(Console.ReadLine());
+sum = firstNumber + secondNumber;
+Console.WriteLine($"The sum is : {Math.Round(sum,2)}");
---
+++ Demo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myFriendsName = "Arun";
+System.Console.WriteLine(myFriendsName);
+
+myFriendsName = "Rahul";
+System.Console.WriteLine(myFriendsName);
